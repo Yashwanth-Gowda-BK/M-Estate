6f5b9a134cb7c1f378c754bb8125c6aeeebe38ef
Yashwanth B K <yashwanthgowdabk1@gmail.com>
2024-02-21 20:19:00 +0530

    create sign up api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -17,5 +18,6 @@ const app = express();
 
 app.listen(3000, () => console.log(`server running at 3000...`));
 
-
+app.use(express.json())
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
